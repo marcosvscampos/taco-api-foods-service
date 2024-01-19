5e6d4b88d67bafb436a7409d7cb4bfdd90370d89
Marcos Campos <contato.marcoscampos@gmail.com>
2024-01-19 10:57:23 -0300

    Alterando verbo do endpoint de sync de alimentos para POST

--- app/controllers/system_controller.py
+++ app/controllers/system_controller.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 from app.services.food_service import FoodService
+#from app.services.food_service_sync import FoodService
 
 router = APIRouter()
 
-@router.get("/system/sync/foods")
+@router.post("/system/sync/foods")
 async def sync_foods():
     service = FoodService()
     response = await service.load_foods()
